a3a65c3f908839ed814a8c197e0ee57e55204ff5
Parnit Sainion <parnits@gmail.com>
2013-09-19 22:05:39 -0700

    Create inital project set up.Created User and Question classes

---
+++ src/com/sjsu/siquoia/Question.java
@@ -0,0 +1,123 @@
+package com.sjsu.siquoia;
+
+import java.util.ArrayList;
+
+/**
+ * @author Parnit Sainion
+ * @since 19 September 2013
+ * Class is used to create a question object with its question, answer choices, correct answer, and the current status of 
+ * whether it was answer or not.
+ *
+ */
+public class Question {
+	
+	private String text;
+	private ArrayList<String> choices = new ArrayList<String>();
+	private int correctChoice;
+	private int status;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+	 * create an empty question object
+	 */
+	 public Question()
+	    {
+	    	//sets status to unanswered
+	    	status = 0;
+	    }
+	 
+	 /**
+	     * @return question text for this object
+	     */
+	    public String getQuestion()
+	    {
+	    	return text;
+	    }
+	    
+	    /**
+	     * Adds a possible answer to the list of choices
+	     * @param choice possible answer to the question
+	     */
+	    public void addChoice(String choice)
+	    {
+	    	choices.add(choice);
+	    }
+	    
+	    /**
+	     * @param position position of choice required
+	     * @return the answer choice for the desired position
+	     */
+	    public String getChoice(int position)
+	    {
+	    	return choices.get(position);
+	    }
+	    
+	    /**
+	     * @return number of choices possible for this question
+	     */
+	    public int numberOfChoices()
+	    {
+	    	return choices.size();
+	    }
+	    /**
+	     * @return the list of choices for this question object
+	     */
+	    public ArrayList<String> getChoices()
+	    {
+	    	return choices;
+	    }
+	    
+	    /**
+	     * @return the position of the correct answer to this question
+	     */
+	    public int getCorrectChoice()
+	    {
+	    	return correctChoice;
+	    }
+	    
+	    /**
+	     * sets the correct answers position
+	     * @param correctChoice position of the correct answer
+	     */
+	    public void setCorrectChoice(int correctChoice)
+	    {
+	    	this.correctChoice = correctChoice;
+	    }
+	   
+	    /**
+	     * sets whether the question has been answered. 
+	     * 0 = unanswered, 1 = correct, 2 = incorrect
+	     * @param num number indicating status of the question
+	     */
+	    public void setStatus(int num)
+	    {
+	    	
+	    	status = num;
+	    }
+	    
+	    /**
+	     * @return status of question
+	     */
+	    public int getAnswered()
+	    {
+	    	return status;
+	    }
+	    
+	    
+	    /**
+	     * @return question text of this object
+	     */
+	    public String toString()
+	    {
+	    	return text;
+	    }
+
+
+}
---
+++ src/com/sjsu/siquoia/User.java
@@ -0,0 +1,101 @@
+package com.sjsu.siquoia;
+
+/**
+ * @author Parnit Sainion
+ * @since 19 September 2013
+ * Class create a user object and store his information.
+ */
+public class User {
+
+	private String name;
+	private int userId;
+	private int siquoiaBucks;
+	private String answers;
+	
+	
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+	 * Create a empty user object
+	 */
+	public User()
+	{
+		
+	}
+	
+	/**
+	 * @return user's name
+	 */
+	public String getName()
+	{
+		return name;
+	}
+	
+	/**
+	 * sets user's name
+	 * @param name new name 
+	 */
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	/**
+	 * @return user's id
+	 */
+	public int getUserId()
+	{
+		return userId;
+	}
+	
+	/**
+	 * sets user's id
+	 * @param userId new user id
+	 */
+	public void setUserId(int userId)
+	{
+		this.userId = userId;
+	}
+	
+	/**
+	 * @return user's siquoia bucks value
+	 */
+	public int getSiquoiaBucks()
+	{
+		return siquoiaBucks;
+	}
+	
+	/**
+	 * sets user's siquoia bucks value
+	 * @param siquoiaBucks
+	 */
+	public void setSequoiaBUcks(int siquoiaBucks)
+	{
+		this.siquoiaBucks = siquoiaBucks;
+	}
+	
+	
+	/**
+	 * @return a string of answers the users has already answered for the quiz
+	 */
+	public String getAnswers()
+	{
+		return answers;
+	}
+	
+	/**
+	 * sets the answers the  user has already answered for the quiz
+	 * @param answers
+	 */
+	public void setAnswers(String answers)
+	{
+		this.answers = answers;
+	}
+}
