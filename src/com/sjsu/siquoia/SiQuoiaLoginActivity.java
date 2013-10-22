b75031e4a173bdd32191cf4c8606039619c76cd0
Parnit Sainion <parnits@gmail.com>
2013-10-22 14:58:00 -0700

    added login activity

--- src/com/sjsu/siquoia/SiQuoiaLoginActivity.java
+++ src/com/sjsu/siquoia/SiQuoiaLoginActivity.java
@@ -5,8 +5,15 @@ package com.sjsu.siquoia;
 
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * @author parnit
@@ -14,12 +21,108 @@ import android.view.Menu;
  */
 public class SiQuoiaLoginActivity extends Activity 
 {
+	private Button loginButton;
+	private ProgressDialog progressBar;
+	private EditText userNameInput, passwordInput;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_screen);
+        
+        //dummy variable for now
+        boolean logggedIn= true;
+        
+        if(logggedIn)//logged in
+        {
+        	Intent intent = new Intent();
+        	intent.setClass(SiQuoiaLoginActivity.this, SiQuoiaHomeActivity.class);
+        	startActivity(intent);
+        	finish();
+        }
+        else
+        {
+        	//get
+        	loginButton = (Button) findViewById(R.id.loginButton);
+        	userNameInput = (EditText) findViewById(R.id.usernameField);
+        	passwordInput = (EditText) findViewById(R.id.passwordField);
+        	
+        	//set up the login button's onClickListener
+        	loginButton.setOnClickListener(new OnClickListener(){
+
+        		//attempts to login the user
+				@Override
+				public void onClick(View arg0) 
+				{
+					new SiQuoiaLoginTask().execute(userNameInput.getText().toString(),passwordInput.getText().toString());
+				}
+        		
+        	});
+        	
+        }
     }
 
+    /**
+     * This is the background task that will log th user into the application by check user credentials on a database.
+     * @author Parnit Sainion
+     *
+     */
+    class SiQuoiaLoginTask extends AsyncTask<String, String, String>
+    {
+    	@Override
+		protected void onPreExecute() {
+			//create the login in progress dialog and display it
+    		progressBar = new ProgressDialog(SiQuoiaLoginActivity.this);
+			progressBar.setIndeterminate(true);
+			progressBar.setCancelable(false);
+			progressBar.setMessage("Logging In");
+			progressBar.show();			
+		}
+    	
+    	@Override
+		protected String doInBackground(String... arg0) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+		protected void OnPostExecute(String result) {
+			
+			//close progress dialog
+			progressBar.dismiss();
+			
+		}    	
+    }
+    
+    /**
+     * This is the background task that will create a new user in the database
+     * @author Parnit Sainion
+     *
+     */
+    class SiQuoiaCreateUserTask extends AsyncTask<String, String, String>
+    {
+    	@Override
+		protected void onPreExecute() {
+			//create the login in progress dialog and display it
+    		progressBar = new ProgressDialog(SiQuoiaLoginActivity.this);
+			progressBar.setIndeterminate(true);
+			progressBar.setCancelable(false);
+			progressBar.setMessage("Creating User");
+			progressBar.show();			
+		}
+    	
+    	@Override
+		protected String doInBackground(String... arg0) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+		protected void OnPostExecute(String result) {
+			
+			//close progress dialog
+			progressBar.dismiss();
+			
+		}    	
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
