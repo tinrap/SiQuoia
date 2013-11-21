271ae6492f964bf03ac950d3a62cb74da44c8fca
Akshay Hegde <akshay.hegde17@gmail.com>
2013-11-20 22:20:11 -0800

    Add getTopic script

---
+++ PHPScripts/getTopic.php
@@ -0,0 +1,17 @@
+<?php
+require_once('database_info.php');
+
+if (isset($_POST['subject'])) {
+    $subject = $_POST['subject'];
+    $query = "SELECT * FROM `Topic` WHERE subject = '". "$subject" . "'";
+    $result = $link->query($query);
+    $json = array();
+
+    if ($result->num_rows !== false) {
+        while ($row = $result->fetch_assoc()) {
+            array_push($json, $row);
+        }
+        echo json_encode($json) . "\n";
+    }
+}
+?>
