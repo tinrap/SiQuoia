05bf2a5460ffd75cdf19d28e601faa31511444e9
Akshay Hegde <akshay.hegde17@gmail.com>
2013-11-20 21:45:22 -0800

    Add new getUser.php script

--- PHPScripts/getUser.php
+++ PHPScripts/getUser.php
@@ -3,20 +3,20 @@ require_once('database_info.php');
 
 if (isset($_POST['email'])) {
     $email    = $_POST['email'];
-    $password = $_POST['password'];
     $json     = array();
 
     // TODO: Will prepare the query to prevent injection later. 
     $query = "SELECT email, currentQuiz, currentAns, siquoiaPoints, packetsBought,";
     $query .= "memorabilia, totalPointsSpent FROM `Users`";
-    $query .= " WHERE email = '" . "$email" . "'AND password = '" . "$password" . "'";
+    $query .= " WHERE email = '" . "$email" "'";
     $result = $link->query($query);
 
     if ($result->num_rows !== false) {
-        echo "true";
-    }
-    else {
-        echo "false";
+        while ($row = $result->fetch_assoc()) {
+            array_push($json, $row);
+        }
+
+        echo json_encode($json);
     }
 }
 ?>
