b97d34f0dedf17bae8da52e7dbf2947155e5f506
Akshay Hegde <akshay.hegde17@gmail.com>
2013-11-24 13:19:54 -0800

    Fix conflicts from revert

--- database/PHPScripts/getQuestions.php
+++ database/PHPScripts/getQuestions.php
@@ -8,27 +8,27 @@ if (isset($_POST['subject'])) {
     $query = "";
     $json = array();
 
-    // Subject not specified in request.
-    if ($subject === "Any") {
-        $query = "select * from `Question` order by rand() limit 20";
-    }
-    // A subject is specified
-    else if ($subject !== "Any" && $topic === "Any") {
-        $query = "select * from `Question` where subject = '";
-        $query .= "$subject" . "' order by rand() limit 20";
-    }
-    // Subject and Topic are specified
-    else if ($subject !== "Any" && $topic !== "Any") {
-        $query = "select * from `Question` where subject = '";
-        $query .= "$subject" . "' and topic = '" . "$topic" . "'";
-        $query .= " order by rand() limit 20";
-    }
-    // All fields are specified
-    else {
+    // All fields specified
+    if ($subtopic !== "Any") {
         $query = "select * from `Question` where subject = '";
         $query .= "$subject" . "' and topic = '" . "$topic" . "'";
         $query .= "and subtopic = '" . "$subtopic" . "'";
         $query .= " order by rand() limit 20";
+    }
+    // Subject and Topic are specified
+    else if ($topic !== "Any") {
+        $query = "select * from `Question` where subject = '";
+        $query .= "$subject" . "' and topic = '" . "$topic" . "'";
+        $query .= " order by rand() limit 20";
+    }
+    // Topic is specified.
+    else if ($subject !== "Any") {
+        $query = "select * from `Question` where subject = '";
+        $query .= "$subject" . "' order by rand() limit 20";
+    }
+    // No fields specified
+    else {
+        $query = "select * from `Question` order by rand() limit 20";
     }
 
     $result = $link->query($query);
