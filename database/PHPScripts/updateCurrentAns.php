550bfe83152c52f23f81f69fc9711654646fc04e
Akshay Hegde <akshay.hegde17@gmail.com>
2013-11-25 22:40:03 -0800

    Fix current Answer query

--- database/PHPScripts/updateCurrentAns.php
+++ database/PHPScripts/updateCurrentAns.php
@@ -3,9 +3,9 @@ require_once('database_info.php');
 
 if (isset($_POST['email'])) {
     $email = $_POST['email'];
-    $currentAns = $_POST['currentAns'];
+    $currentAns = $_POST['currentAnswers'];
 
-    $stmt = $link->prepare("update Users currentAns = ? where email = email = ?");
+    $stmt = $link->prepare("update Users set currentAns = ? where email = ?");
     $stmt->bind_param('ss', $currentAns, $email);
     $stmt->execute();
 }
