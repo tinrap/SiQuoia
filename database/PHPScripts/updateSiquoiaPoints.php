85b9688f21a37b83e46eb6240804977dd4705808
Akshay Hegde <akshay.hegde17@gmail.com>
2013-11-27 19:12:20 -0800

    Add update Siquoia points script

---
+++ database/PHPScripts/updateSiquoiaPoints.php
@@ -0,0 +1,12 @@
+<?php
+require_once('database_info.php');
+
+if (isset($_POST['points'])) {
+    $points = $_POST['points'];
+    $email  = $_POST['email'];
+
+    $stmt = $link->prepare("update Users set siquoiaPoints = ? where email = ?");
+    $stmt->bind_param("ss", $points, $email);
+    $stmt->execute();
+}
+?>
