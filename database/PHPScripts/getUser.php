cdc1d1ed160bde819bee5cd5f1a55609e3bdda75
Akshay Hegde <akshay.hegde17@gmail.com>
2013-11-27 19:07:17 -0800

    Update getUser.php to update packetType

--- database/PHPScripts/getUser.php
+++ database/PHPScripts/getUser.php
@@ -7,8 +7,8 @@ if (isset($_POST['email'])) {
 
     // TODO: Will prepare the query to prevent injection later. 
     $query = "SELECT email, currentQuiz, currentAns, siquoiaPoints, packetsBought,";
-    $query .= "memorabilia, totalPointsSpent FROM `Users`";
-    $query .= " WHERE email = '" . "$email" "'";
+    $query .= "memorabilia, totalPointsSpent, packetType FROM `Users`";
+    $query .= " WHERE email = '" . "$email" . "'";
     $result = $link->query($query);
 
     if ($result->num_rows !== false) {
