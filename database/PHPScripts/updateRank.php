0b81473b5cd420b00f0b91fabd86bf735e8a8729
Akshay Hegde <akshay.hegde17@gmail.com>
2013-11-28 17:16:28 -0800

    Fix updating rank

--- database/PHPScripts/updateRank.php
+++ database/PHPScripts/updateRank.php
@@ -4,7 +4,7 @@ require_once('database_info.php');
 if (isset($_POST['questionText'])) {
     $questionText = $_POST['questionText'];
 
-    $stmt = $link->prepare("update Question set rank = ? where questionText = ?");
+    $stmt = $link->prepare("update Question set rank = rank + ? where questionText = ?");
     $number = 1;
     $stmt->bind_param('ds', $number, $questionText);
     $stmt->execute();
